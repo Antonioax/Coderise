db24159e7c0b877339b859fcad7ba4b97680148e
Antonio Jurić <juricantonio18@gmail.com>
2024-06-25 21:20:09 +0200

    Added routing and fixed design issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ContactComponent } from "./components/contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'About',
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    title: 'Contact',
+  },
+];
