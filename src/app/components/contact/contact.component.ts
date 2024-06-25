89ba8adcc528038188b1cff551274673cdac033a
Antonio Jurić <juricantonio18@gmail.com>
2024-06-25 23:13:33 +0200

    Finished initial contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormsModule, NgForm, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [],
   templateUrl: './contact.component.html',
 })
 export class ContactComponent {
-  form!: FormGroup;
-
-  onSend(){
-
-  }
 }
