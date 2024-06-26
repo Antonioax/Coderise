5bcd734f38739f7dde5a6e1c8c0d190163d80561
Antonio Jurić <juricantonio18@gmail.com>
2024-06-26 13:50:22 +0200

    Updated dynamic content workflow

--- src/app/content/home.content.ts
+++ src/app/content/home.content.ts
@@ -1,28 +1,28 @@
 export interface Content {
-  title: string | null;
-  content: string | null;
-  image: string | null;
+  title?: string;
+  content?: string;
+  image?: string;
 }
 
 export const HomeContent: Content[] = [
   {
     title: 'Static Websites',
     content: `We specialize in crafting professional static websites tailored to elevate your business. Our solutions are designed to provide a seamless online presence, ensuring your brand stands out. Trust us to deliver high-quality, efficient websites that enhance your digital footprint.`,
-    image: null,
+  },
+  {
+    title: 'Static websites',
+    image: 'assets/images/static.jpg',
   },
   {
     title: 'Dynamic Web Apps',
     content: `We develop dynamic web applications that put you in complete control. Our cutting-edge solutions are designed to offer flexibility and functionality, empowering your business to thrive online. Experience seamless, user-friendly applications tailored to your specific needs.`,
-    image: null,
   },
   {
     title: 'Mobile Apps',
     content: `We create innovative mobile apps that enhance your business’s reach and engagement. Our mobile solutions are tailored to deliver a seamless user experience, ensuring your brand stays connected with customers on the go. Trust us to develop high-quality, intuitive apps that drive your business forward.`,
-    image: null,
   },
   {
     title: 'Digitalizing Businesses',
     content: `We specialize in digitalizing products and businesses to drive growth and efficiency. Our comprehensive solutions transform your offerings, ensuring they are competitive in the digital landscape. Partner with us to innovate and elevate your business through cutting-edge digital transformation.`,
-    image: null,
   },
 ];
