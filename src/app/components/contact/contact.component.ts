a810b753806109bd3d3fdc9744d7a471b94556d3
Antonio Jurić <juricantonio18@gmail.com>
2024-06-28 17:56:03 +0200

    Improved SEO tags for home and contact

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,16 +1,33 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './contact.component.html'
+  templateUrl: './contact.component.html',
 })
 export class ContactComponent implements OnInit {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private meta: Meta,
+    private title: Title
+  ) {}
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) document.body.scrollTop = 0;
+    this.title.setTitle('coderise - Contact');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content:
+          'Get in touch with coderise team to fulfill your project idea.',
+      },
+      {
+        name: 'keywords',
+        content: 'coderise, Coderise, software, website, application, contact',
+      },
+    ]);
   }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Content, HomeContent } from '../../content/home.content';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -12,12 +13,22 @@ import { CommonModule, isPlatformBrowser } from '@angular/common';
 export class HomeComponent implements OnInit {
   content!: Content[];
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object){
-    
-  }
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private meta: Meta,
+    private title: Title
+  ) {}
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) document.body.scrollTop = 0;
+    this.title.setTitle('coderise - Home');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: 'Everything you need to know about coderise.',
+      },
+      { name: 'keywords', content: 'coderise, Coderise, software, website, application' },
+    ]);
     this.content = HomeContent;
   }
 }
